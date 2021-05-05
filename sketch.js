3ab780da274af234c542be11329367f37b26a4ac
shubhamtamta27 <77672941+shubhamtamta27@users.noreply.github.com>
2021-05-05 17:30:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    rubber = new Rubber(600, 300, 40);
+    stone = new Stone(400, 280);
+    iron = new Iron(200, 340);
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rubber.display();
+    stone.display();
+    iron.display();
+}
---
+++ stone.js
@@ -0,0 +1,25 @@
+class Stone {
+    constructor(x, y) {
+      var options = {
+        'density':12,
+        'friction': 0.9,
+        'restitution':0.8
+      };
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 100;
+      this.height = 40;
+      World.add(world, this.body);
+    };
+    display(){
+      var pos = this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      strokeWeight(3);
+      stroke('white')
+      fill('black')
+      rectMode(CENTER)
+      rect(0, 0, this.width, this.height);
+      pop();
+    };
+  };
+  
